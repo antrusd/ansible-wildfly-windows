54eacb88bbe14ec89772d83efa507fd825293582
Anthony Rusdi <33247310+antrusd@users.noreply.github.com>
2019-11-10 21:00:07 +0700

    use double quote for sh and use unix file format
    Signed-off-by: Anthony Rusdi <33247310+antrusd@users.noreply.github.com>

--- Jenkinsfile
+++ Jenkinsfile
@@ -9,28 +9,28 @@ pipeline {
     stages {
         stage('Clean Workspace') {
             steps {
-                sh 'git clean -xdff'
+                sh "git clean -xdff"
             }
         }
 
         stage('Install Ansible') {
             steps {
-                sh 'virtualenv --system-site-packages -p python3 ${WORKSPACE}/ansible-tf-azure'
+                sh "virtualenv --system-site-packages -p python3 ${WORKSPACE}/ansible-tf-azure"
 
                 withPythonEnv("${WORKSPACE}/ansible-tf-azure/") {
-                    sh 'pip install --upgrade pip'
-                    sh 'pip install -r requirements.txt'
+                    sh "pip install --upgrade pip"
+                    sh "pip install -r requirements.txt"
                 }
             }
         }
 
         stage('Installing Terraform') {
             steps {
-                sh 'curl -o ${WORKSPACE}/terraform.zip https://releases.hashicorp.com/terraform/0.12.12/terraform_0.12.12_linux_amd64.zip'
-                sh 'unzip -o -d ${WORKSPACE}/ansible-tf-azure/bin/ ${WORKSPACE}/terraform.zip'
-                sh 'rm -f ${WORKSPACE}/terraform.zip'
+                sh "curl -o ${WORKSPACE}/terraform.zip https://releases.hashicorp.com/terraform/0.12.12/terraform_0.12.12_linux_amd64.zip"
+                sh "unzip -o -d ${WORKSPACE}/ansible-tf-azure/bin/ ${WORKSPACE}/terraform.zip"
+                sh "rm -f ${WORKSPACE}/terraform.zip"
                 withCredentials([string(credentialsId: 'ARM_CLIENT_ID', variable: 'ARM_CLIENT_ID'), string(credentialsId: 'ARM_CLIENT_SECRET', variable: 'ARM_CLIENT_SECRET'), string(credentialsId: 'ARM_SUBSCRIPTION_ID', variable: 'ARM_SUBSCRIPTION_ID'), string(credentialsId: 'ARM_TENANT_ID', variable: 'ARM_TENANT_ID')]) {
-                    sh '${WORKSPACE}/ansible-tf-azure/bin/terraform init -input=false'
+                    sh "${WORKSPACE}/ansible-tf-azure/bin/terraform init"
                 }
             }
         }
--- variables.tf
+++ variables.tf
@@ -1,83 +1,82 @@
 variable "region" {
-  description = "Deployment region"
-  default = "Central Us"
-}
+  description = "Deployment region"
+  default = "Central Us"
+}
 
-variable "VMRG" {
-  description = "Resource group to deploy VM"
-  default = "SHW-BBCT-CICD-RG"
-}
-
-variable "vm_name" {
-    description = "Enter desired name of VM"
-    type = "string"
-    default = "WIN-VM-CUS"
-
-}
+variable "VMRG" {
+  description = "Resource group to deploy VM"
+  default = "SHW-BBCT-CICD-RG"
+}
 
-variable "vm_size" {
-  description = "Enter the Size of VMs"
-  default = "Standard_D4_v3"
-}
-
-variable "admin_username" {
-  description = "Admin user name for VM"
-  default = "vignoadmin "
-}
-
-variable "count_of_VMs" {
-  description = "Number of VMs you want to create as part of this deployment"
-  #type = "string"
-  default = 2
-}
+variable "vm_name" {
+  description = "Enter desired name of VM"
+  type = "string"
+  default = "WIN-VM-CUS"
+}
 
-variable "VnetRG" {
-  description = "Name of Virtual network resource group"
-  default = "SHAWAUTH-VIGNO-CICD-T-RG"
-}
-variable "VnetName" {
-  description = "Name of Virtual Network"
-  default = "SHAW-AUTH-VIGNO-CICD-T-vnet"
-}
-variable "SubnetName" {
-  description = "Name of Subnet"
-  default = "shaw-auth-vigno-cicd-t-sn"
-}
-
-variable "KeyVaultName" {
-  description = "Name of Key Vault"
-  default = "shwvignointervault"
-}
+variable "vm_size" {
+  description = "Enter the Size of VMs"
+  default = "Standard_D4_v3"
+}
 
-variable "KeyVaultRG" {
-  description = "Resource group of key vault"
-  default = "SHW-INT-VIGCICD-RG"
-}
-
-variable "BootDiagnosticsStorageAccount" {
-  description = "Storage account to store VM boot diagnostics"
-  default = "sysbootdiag"
-}
-
-variable "BootDiagnosticsStorageResourceGroup" {
-  description = "Resource group of boot diagnostics storage account"
-  default = "SHW-INT-VIGCICD-RG"
-}
-
-variable "OS_Image_Publisher" {
-  description = "Give OS image with which you need to create virtual machines"
-  type = "string"
-  default = "MicrosoftWindowsServer"
-}
+variable "admin_username" {
+  description = "Admin user name for VM"
+  default = "vignoadmin"
+}
 
-variable "OS_Image_Offer" {
-  description = "Provide the name of offer for the given publisher"
-  type= "string"
-  default = "WindowsServer"
-}
+variable "count_of_VMs" {
+  description = "Number of VMs you want to create as part of this deployment"
+  #type = "string"
+  default = 2
+}
 
-variable "OS_Image_Sku" {
-  description = "Provide the version of sku. Ex:- 2019-Datacenter"
-  type = "string"
-  default = "2016-Datacenter"
-}
+variable "VnetRG" {
+  description = "Name of Virtual network resource group"
+  default = "SHAWAUTH-VIGNO-CICD-T-RG"
+}
+variable "VnetName" {
+  description = "Name of Virtual Network"
+  default = "SHAW-AUTH-VIGNO-CICD-T-vnet"
+}
+variable "SubnetName" {
+  description = "Name of Subnet"
+  default = "shaw-auth-vigno-cicd-t-sn"
+}
+
+variable "KeyVaultName" {
+  description = "Name of Key Vault"
+  default = "shwvignointervault"
+}
+
+variable "KeyVaultRG" {
+  description = "Resource group of key vault"
+  default = "SHW-INT-VIGCICD-RG"
+}
+
+variable "BootDiagnosticsStorageAccount" {
+  description = "Storage account to store VM boot diagnostics"
+  default = "sysbootdiag"
+}
+
+variable "BootDiagnosticsStorageResourceGroup" {
+  description = "Resource group of boot diagnostics storage account"
+  default = "SHW-INT-VIGCICD-RG"
+}
+
+variable "OS_Image_Publisher" {
+  description = "Give OS image with which you need to create virtual machines"
+  type = "string"
+  default = "MicrosoftWindowsServer"
+}
+
+variable "OS_Image_Offer" {
+  description = "Provide the name of offer for the given publisher"
+  type= "string"
+  default = "WindowsServer"
+}
+
+variable "OS_Image_Sku" {
+  description = "Provide the version of sku. Ex:- 2019-Datacenter"
+  type = "string"
+  default = "2016-Datacenter"
+}
